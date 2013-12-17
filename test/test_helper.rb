a3d2d97d5a284ff23eab90f239a9b186d301ee5e
Nilesh Chaudhari <mail@nilesh.org>
2013-12-17 12:22:27 +0530

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,4 @@ require 'active_support'
 ENV["RAILS_ENV"] = "test"
 require 'action_pack'
 require 'action_controller'
-require File.expand_path('../../lib/rails3_before_render', __FILE__)
+require File.expand_path('../../lib/before_render', __FILE__)
